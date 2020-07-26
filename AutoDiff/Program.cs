ee71bdc27570d2d641a5cbff7a0d64d5821d07cb
byx2000 <416982860@qq.com>
2020-07-26 12:54:10 +0800

    添加使用示例

--- AutoDiff/Program.cs
+++ AutoDiff/Program.cs
@@ -9,19 +9,8 @@ namespace AutoDiff
 
         static void Main()
         {
-            // 用梯度下降法求函数y = x * x的最小值 (0, 0)
-
-            Node x = (rand.NextDouble() * 2 - 1) * 1000;
-            Node y = x * x;
-
-            double rate = 0.1;
-            for (int i = 0; i < 100; ++i)
-            {
-                y.Forward();
-                Console.WriteLine("x = " + x.Value + "\ty = " + y.Value);
-                y.Backward();
-                x.Value -= rate * x.Derivative;
-            }
+            Sample.RunSample1();
+            Sample.RunSample2();
 
             Console.WriteLine("请按任意键继续...");
             Console.ReadKey();
---
+++ AutoDiff/Sample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDiff
+{
+    public static class Sample
+    {
+        private static Random rand = new Random();
+
+        /// <summary>
+        /// 用梯度下降法求函数y=x^2的最小值
+        /// </summary>
+        public static void RunSample1()
+        {
+            Console.WriteLine("Sample 1:");
+
+            Node x = (rand.NextDouble() * 2 - 1) * 1000;
+            Node y = x * x;
+
+            double rate = 0.1;
+            int epoch = 100;
+            for (int i = 0; i < epoch; ++i)
+            {
+                y.Forward();
+                Console.WriteLine("x = " + x.Value + "\ty = " + y.Value);
+                y.Backward();
+                x.Value -= rate * x.Derivative;
+            }
+        }
+
+        /// <summary>
+        /// 拟合直线y=2.5x-3.7
+        /// </summary>
+        public static void RunSample2()
+        {
+            Console.WriteLine("Sample 2:");
+
+            // 构造训练数据：y=2.5x-3.7
+            List<double> x = new List<double>();
+            List<double> y = new List<double>();
+            for (double i = -5; i <= 5; i += 0.1)
+            {
+                x.Add(i);
+                y.Add(2.5 * i - 3.7);
+            }
+
+            Node loss = 0;
+            Node k = rand.NextDouble();
+            Node b = rand.NextDouble();
+            for (int i = 0; i < x.Count; ++i)
+            {
+                Node delta = k * x[i] + b - y[i];
+                loss += delta * delta;
+            }
+
+            double rate = 0.001;
+            int epoch = 100;
+            for (int i = 0; i < epoch; ++i)
+            {
+                loss.Forward();
+                Console.WriteLine("y=(" + k.Value + ")x+(" + b.Value + ")\tloss=" + loss.Value);
+                loss.Backward();
+                k.Value -= rate * k.Derivative;
+                b.Value -= rate * b.Derivative;
+            }
+        }
+    }
+}
