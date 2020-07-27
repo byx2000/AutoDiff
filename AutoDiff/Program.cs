e5b83a46e1adef319d5d6432d150fb03e861e109
byx2000 <416982860@qq.com>
2020-07-27 12:03:37 +0800

    抽象BinaryNode二元运算节点基类

--- AutoDiff/Program.cs
+++ AutoDiff/Program.cs
@@ -10,18 +10,6 @@ namespace AutoDiff
             Sample.RunSample1();
             Sample.RunSample2();
 
-            Var r = 3;
-            Const pi = 3.14;
-            Node s = pi * r * r;
-
-            s.Forward();
-            Console.WriteLine(s.Value);
-
-            s.Backward();
-            Console.WriteLine(r.Derivative);
-            Console.WriteLine(pi.Derivative);
-            
-
             Console.WriteLine("请按任意键继续...");
             Console.ReadKey();
         }
